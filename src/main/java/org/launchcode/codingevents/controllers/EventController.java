49b77a302dc9806d8515368613256b575b3e0937
anicraj456 <anicraj123@gmail.com>
2024-02-04 17:23:04 -0600

    Hibernate prac

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,8 +2,10 @@ package org.launchcode.codingevents.controllers;
 
 import jakarta.validation.Valid;
 import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.data.EventRepository;
 import org.launchcode.codingevents.models.Event;
 import org.launchcode.codingevents.models.EventType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
@@ -18,11 +20,13 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+    @Autowired
+    private EventRepository repository;
 
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", repository.findAll());
         return "events/index";
     }
 
@@ -42,14 +46,14 @@ public class EventController {
             return "events/create";
         }
 
-        EventData.add(newEvent);
+        repository.save(newEvent);
         return "redirect:/events";
     }
 
     @GetMapping("delete")
     public String displayDeleteEventForm(Model model) {
         model.addAttribute("title", "Delete Events");
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", repository.findAll());
         return "events/delete";
     }
 
@@ -58,7 +62,7 @@ public class EventController {
 
         if (eventIds != null) {
             for (int id : eventIds) {
-                EventData.remove(id);
+                repository.deleteById(id);
             }
         }
 
